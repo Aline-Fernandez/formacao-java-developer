62af8cfdebda006cd2ac3710b43367ae33b56500
Aline Fernandes <fernandes.dev@outlook.com>
2026-03-08 21:26:52 -0300

    Arquivos da Formação Java Developer

---
+++ src/MinhaClasse.java
@@ -0,0 +1,28 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+//        // exemplo 1
+//        String primeiroNome = "Aline"
+//        String segundoNome = "Fernandes";
+////
+////        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+////        System.out.println(nomeCompleto);
+////    }
+////
+////    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+////        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+////         }
+//
+//        // exemplo 2
+//
+//        String MeuNome = "Aline";
+//
+//        int anoFabricacao = 2022;
+//
+//        boolean verdadeira = false;
+//
+//        anoFabricacao = 2018;
+    }
+
+}
