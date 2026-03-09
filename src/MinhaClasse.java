5299c1c9887867c835563c814a7d86063e437b3a
Aline Fernandes <fernandes.dev@outlook.com>
2026-03-08 21:50:08 -0300

    Aula 3 usando concat

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,28 +1,16 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
+        String primeiroNome = "Aline";
+        String segundoNome = "Fernandes";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
-//        // exemplo 1
-//        String primeiroNome = "Aline"
-//        String segundoNome = "Fernandes";
-////
-////        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-////        System.out.println(nomeCompleto);
-////    }
-////
-////    public static String nomeCompleto(String primeiroNome, String segundoNome) {
-////        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
-////         }
-//
-//        // exemplo 2
-//
-//        String MeuNome = "Aline";
-//
-//        int anoFabricacao = 2022;
-//
-//        boolean verdadeira = false;
-//
-//        anoFabricacao = 2018;
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
