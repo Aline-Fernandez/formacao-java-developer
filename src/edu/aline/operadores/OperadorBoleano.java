181bf1d053e1a377d60d5b4f4a429800894b29d8
Aline Fernandes <fernandes.dev@outlook.com>
2026-04-06 17:57:42 -0300

    operadores encremento e boleano

---
+++ src/edu/aline/operadores/OperadorBoleano.java
@@ -0,0 +1,11 @@
+package edu.aline.operadores;
+
+public class OperadorBoleano {
+    public static void main(String[] args){
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+}
