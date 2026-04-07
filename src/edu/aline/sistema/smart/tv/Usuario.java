b01b15f30147b7cec31e430db6119bfaca2b2140
Aline Fernandes <109555882+aline-fernandez@users.noreply.github.com>
2026-04-07 11:32:00 -0300

    exercício Sistema Smarte TV

---
+++ src/edu/aline/sistema/smart/tv/Usuario.java
@@ -0,0 +1,32 @@
+package edu.aline.sistema.smart.tv;
+
+public class Usuario {
+
+    public static void main(String[] args){
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal atual da TV: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal atual da TV : " + smartTv.canal);
+
+
+        System.out.println("Volume atual da TV : " + smartTv.volume);
+
+
+        System.out.println("TV ligada ? " + smartTv.ligada);
+        System.out.println("Canal atual da TV: " + smartTv.canal);
+        System.out.println("Volume atual TV : " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV ligada ? " + smartTv.ligada);
+
+    }
+}
