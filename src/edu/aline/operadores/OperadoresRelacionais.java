8f01fb9c92db35931a55c9eed2a43eb21b88c171
Aline Fernandes <fernandes.dev@outlook.com>
2026-04-07 08:46:42 -0300

    operadores relacionais

--- src/edu/aline/operadores/OperadoresRelacionais.java
+++ src/edu/aline/operadores/OperadoresRelacionais.java
@@ -16,7 +16,7 @@ public class OperadoresRelacionais {
         boolean simNao = numero1 == numero2;
 
         if (numero1 < numero2){
-            System.out.println("a nossa condição é verdadeira");
+            System.out.println("a nossa condição é verdadeira ");
         }
 
         System.out.println("numero1 é igual numero2 ? " + simNao);
