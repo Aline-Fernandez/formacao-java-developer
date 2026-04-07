5dd2393dfcdb7c5d3bb7220edb5739e465c1e6bf
Aline Fernandes <fernandes.dev@outlook.com>
2026-04-07 08:55:44 -0300

    operadores lógicos

---
+++ src/edu/aline/operadores/OreradoresLogicos.java
@@ -0,0 +1,20 @@
+package edu.aline.operadores;
+
+public class OreradoresLogicos {
+
+    public static void main(String[] args){
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if (condicao1 || condicao2){
+            System.out.println("Uma condição é verdadeira");
+        }
+
+        System.out.println("fim");
+
+    }
+}
